cdb0a0a3f3e7f865965ab7411a5e0b17700f54f2
권혁준 <gurwnsdl1@gwonhyeogjun-ui-MacBookAir.local>
2019-08-25 22:50:09 +0900

    0825
    naver 지도 뜨기

---
+++ Mohae/Mohae/Header.h
@@ -0,0 +1,7 @@
+#ifndef your_project_Bridging_Header_h
+#define your_project_Bridging_Header_h
+
+#import <NMapViewerSDK/NMapView.h>
+#import <NMapViewerSDK/NMapLocationManager.h>
+
+#endif
--- Mohae/Mohae/ViewController.swift
+++ Mohae/Mohae/ViewController.swift
@@ -8,11 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NMapViewDelegate, NMapPOIdataOverlayDelegate {
+    
+    
+     var hasInit = false
+    var mapView: NMapView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.navigationController?.navigationBar.isTranslucent = false
+        mapView = NMapView(frame: self.view.frame)
+        
+        if let mapView = mapView {
+            // set the delegate for map view
+            mapView.delegate = self            // set the application api key for Open MapViewer Library
+            mapView.setClientId("r8s8v3u0v7")
+            mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            view.addSubview(mapView)
+        }
     }
+    
+    func onMapView(_ mapView: NMapView!, initHandler error: NMapError!) {
+        if (error == nil) { // success
+            // set map center and level
+            mapView.setMapCenter(NGeoPoint(longitude:126.978371, latitude:37.5666091), atLevel:11)
+            // set for retina display
+            mapView.setMapEnlarged(true, mapHD: true)
+            // set map mode : vector/satelite/hybrid
+            mapView.mapViewMode = .vector
+            
+            hasInit = true
+        } else { // fail
+            print("onMapView:initHandler: \(error.description)")
+        }
+    }
+    
+    open func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, imageForOverlayItem poiItem: NMapPOIitem!, selected: Bool) -> UIImage! {
+        return NMapViewResources.imageWithType(poiItem.poiFlagType, selected: selected)
+    }
+    open func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, anchorPointWithType poiFlagType: NMapPOIflagType) -> CGPoint {
+        return NMapViewResources.anchorPoint(withType: poiFlagType)
+    }
+    open func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, calloutOffsetWithType poiFlagType: NMapPOIflagType) -> CGPoint {
+        return CGPoint(x: 0, y: 0)
+    }
+    open func onMapOverlay(_ poiDataOverlay: NMapPOIdataOverlay!, imageForCalloutOverlayItem poiItem: NMapPOIitem!, constraintSize: CGSize, selected: Bool, imageForCalloutRightAccessory: UIImage!, calloutPosition: UnsafeMutablePointer<CGPoint>!, calloutHit calloutHitRect: UnsafeMutablePointer<CGRect>!) -> UIImage! {
+        return nil
+    }
+    
 }
 
